7e20c4e71de113bc92994e60221af1bf792f4c46
MarceloAndresMendoza <marcelo@doblefoco.cl>
2023-06-17 11:21:25 -0400

    Refactoring code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,40 @@
-const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
-        datasets: [{
-            label: '# de solicitudes resueltas',
-            data: [35, 37, 27, 23, 27, 22, 38],
-            borderWidth: 1
-        },
-        {
-            label: '# de solicitudes recibidas',
-            data: [39, 42, 34, 29, 28, 26, 39],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: false
-            }
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const dataTotals = {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+        label: '# de solicitudes totales',
+        values: [35, 37, 27, 23, 27, 38],
+        borderWidth: 1
     }
+    const dataResolved = {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+        label: '# de solicitudes resueltas',
+        values: [27, 29, 23, 18, 25, 37],
+        borderWidth: 1
+    }
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dataTotals.labels,
+            datasets: [
+                {
+                    label: dataTotals.label,
+                    data: dataTotals.values,
+                    borderWidth: 1,
+                },{
+                    label: dataResolved.label,
+                    data: dataResolved.values,
+                    borderWidth: 1,
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false,
+                }
+            }
+        }}
+    );
 });
